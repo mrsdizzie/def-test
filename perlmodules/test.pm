efae8547bf44240b9f6c4caecf43ff78346606c7
Isla Waters <info@mrsdizzie.com>
2023-11-08 11:49:55 -0500

    first commit

---
+++ perlmodules/test.pm
@@ -0,0 +1,10 @@
+package XX::Test2;
+
+use strict;
+use warnings;
+
+sub say {
+    my $message = shift;
+    print "$message\n";
+}
+say();
---
+++ test2.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+sub test2 {
+    print "test\n";
+}
+test2();
